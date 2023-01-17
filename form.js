a52115e06a4de0ae65606ad2c2b5f59d83c52c96
jorge-ariznabarreta-zabala <jorge.ariznabarreta.zabala@gmail.com>
2023-01-17 12:01:41 +0100

    funcionaJS

--- form.js
+++ form.js
@@ -5,18 +5,18 @@ let posFinal=9;
 // const boton=document.getElementById("recogeDatos");
 // boton.addEventListener('click', recogeDatos());
 
-function recogeDatos() {
+function RecogeDatos() {
     posInicial=document.getElementById("firstNumber").value;//name="dopetrope-tropedope"
-    console.log(posInicial);
+    //console.log(posInicial);
     posFinal=document.getElementById("lastNumber").value; //name="seleccion"
-    console.log(posFinal);
+    //console.log(posFinal);
     RecorreArray(posInicial, posFinal);
-}
+};
 function RecorreArray(n1, n2) {
    for (let index = n1; index <= n2; index++) {
     const element = arrayLetras[index];
-    console.log(element);
-    //document.getElementById("cajatexto").innerHTML = element;
+    //console.log(element);
+    document.getElementById("cajatexto").innerHTML += element;
    } 
-}
-RecorreArray(posInicial, posFinal);
+};
+// RecorreArray(posInicial, posFinal);
