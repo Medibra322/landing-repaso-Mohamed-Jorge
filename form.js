c1c5d8bcb55394edd8bc6f4651c9c69ab30334f0
jorge-ariznabarreta-zabala <jorge.ariznabarreta.zabala@gmail.com>
2023-01-17 10:02:03 +0100

    finalConJS

--- form.js
+++ form.js
@@ -1,12 +1,12 @@
 let arrayLetras=['a','b','c','d','e','f','g','h','i','j'];
-let posInicial;
-let posFinal;
+let posInicial =0;
+let posFinal=9;
 
-const button=document.getElementById("recogeDatos");
-button.addEventListener('click', recogeDatos());
+// const boton=document.getElementById("recogeDatos");
+// boton.addEventListener('click', recogeDatos());
 
 function recogeDatos() {
-    posInicial=document.getElementById("firstNumber");//name="dopetrope-tropedope"
+    posInicial=document.getElementById("firstNumber").value;//name="dopetrope-tropedope"
     console.log(posInicial);
     posFinal=document.getElementById("lastNumber").value; //name="seleccion"
     console.log(posFinal);
@@ -16,5 +16,7 @@ function RecorreArray(n1, n2) {
    for (let index = n1; index <= n2; index++) {
     const element = arrayLetras[index];
     console.log(element);
+    //document.getElementById("cajatexto").innerHTML = element;
    } 
 }
+RecorreArray(posInicial, posFinal);
